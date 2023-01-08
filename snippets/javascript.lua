15aec4b72b7952509bb91eccf8ea76a36a16c530
Seb Jones <seb.jones@enovate.co.uk>
2023-01-08 16:16:48 +0000

    Use fmt() for existing snippets

--- snippets/javascript.lua
+++ snippets/javascript.lua
@@ -1,11 +1,11 @@
 return {
-	s("cl", { t("console.log("), i(1), t(")") }),
+	s("cl", fmt("console.log({})", { i(1) })),
 
-	s("ce", { t("console.error("), i(1), t(")") }),
+	s("ce", fmt("console.error({})", { i(1) })),
 
-    s("if", {
-        t("if ("), i(1), t(") {"),
-        t({ "", "    " }), i(2),
-        t({ "", "}" }),
-    }),
+    s("if", fmt([[
+        if ({}) {{
+            {}
+        }}
+    ]], { i(1), i(2) })),
 }
